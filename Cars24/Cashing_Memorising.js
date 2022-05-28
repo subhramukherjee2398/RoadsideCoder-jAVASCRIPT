06ce494bd2842e8805388dd361b8aa27dcbe9f21
subhraWingsure <subhra@wingsure.co>
2022-05-28 10:21:53 +0530

    incomplete preparation...

---
+++ Cars24/Cashing_Memorising.js
@@ -0,0 +1,19 @@
+
+
+// memorize the function
+
+
+function square(n1 , n2){
+   for(let i = 0 ; i<1000000;i++){
+
+   }
+   return n1 * n2
+}
+
+console.time("1st time")
+console.log(square(123654,7894561))
+console.timeEnd("1st time")
+
+// solution
+
+
