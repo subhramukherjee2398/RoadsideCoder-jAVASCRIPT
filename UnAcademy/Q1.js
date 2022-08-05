4408abf91ffb93c7f8eec488b2aea1146ac0cf9c
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-08-05 22:01:21 +0530

    monor

--- UnAcademy/Q1.js
+++ UnAcademy/Q1.js
@@ -1,5 +1,5 @@
 
 
-console.log(null == undefined)
+console.log(null == undefined)  // false == false
 
-console.log(null === undefined)
+console.log(null === undefined)  // typeOf(null) {object} === typeof(undefined) {undefined} 
